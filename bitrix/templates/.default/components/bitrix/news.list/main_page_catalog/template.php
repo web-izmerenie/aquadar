255f26f8f261651f03ca3ef7d79b8009d605b0c6
Andrey Chechkin <andrey.chechkin@web-izmerenie.ru>
2015-01-29 16:39:39 +0300

    Backend main page catalog section

---
+++ bitrix/templates/.default/components/bitrix/news.list/main_page_catalog/template.php
@@ -0,0 +1,23 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+
+<div id="main-bottom">
+		<div class="content-frame">
+
+<?foreach($arResult["ITEMS"] as $arItem):?>
+
+
+			<a href="#" class="main-items">
+				<img src="<?=$arItem["PREVIEW_PICTURE"]["SRC"];?>">
+				<div class="circle-description" id="<?=$arItem["PROPERTIES"]["ATT_COLOR_BACKGROUND"]["VALUE_XML_ID"];?>">
+					<div class="circle-content">
+						<h1><?=$arItem["NAME"];?></h1>
+						<p><?=$arItem["PROPERTIES"]["ATT_CORPORATION"]["VALUE"];?></p>
+						<?=$arItem["PREVIEW_TEXT"];?>
+					</div>
+				</div>
+			</a>
+
+<?endforeach;?>
+
+		</div>
+	</div>
--- index.php
+++ index.php
@@ -1,91 +1,91 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Аквадар");
-?><?$APPLICATION->IncludeComponent("bitrix:news.detail", "main_page_top", Array(
-	"IBLOCK_TYPE" => "content",	// Тип информационного блока (используется только для проверки)
-	"IBLOCK_ID" => "7",	// Код информационного блока
-	"ELEMENT_ID" => "45",	// ID новости
-	"ELEMENT_CODE" => "",	// Код новости
-	"CHECK_DATES" => "Y",	// Показывать только активные на данный момент элементы
-	"FIELD_CODE" => array(	// Поля
-		0 => "",
-		1 => "",
-	),
-	"PROPERTY_CODE" => array(	// Свойства
-		0 => "",
-		1 => "",
-	),
-	"IBLOCK_URL" => "",	// URL страницы просмотра списка элементов (по умолчанию - из настроек инфоблока)
-	"AJAX_MODE" => "N",	// Включить режим AJAX
-	"AJAX_OPTION_JUMP" => "N",	// Включить прокрутку к началу компонента
-	"AJAX_OPTION_STYLE" => "Y",	// Включить подгрузку стилей
-	"AJAX_OPTION_HISTORY" => "N",	// Включить эмуляцию навигации браузера
-	"CACHE_TYPE" => "A",	// Тип кеширования
-	"CACHE_TIME" => "36000000",	// Время кеширования (сек.)
-	"CACHE_GROUPS" => "Y",	// Учитывать права доступа
-	"META_KEYWORDS" => "-",	// Установить ключевые слова страницы из свойства
-	"META_DESCRIPTION" => "-",	// Установить описание страницы из свойства
-	"BROWSER_TITLE" => "-",	// Установить заголовок окна браузера из свойства
-	"SET_STATUS_404" => "N",	// Устанавливать статус 404, если не найдены элемент или раздел
-	"SET_TITLE" => "N",	// Устанавливать заголовок страницы
-	"INCLUDE_IBLOCK_INTO_CHAIN" => "N",	// Включать инфоблок в цепочку навигации
-	"ADD_SECTIONS_CHAIN" => "N",	// Включать раздел в цепочку навигации
-	"ADD_ELEMENT_CHAIN" => "N",	// Включать название элемента в цепочку навигации
-	"ACTIVE_DATE_FORMAT" => "",	// Формат показа даты
-	"USE_PERMISSIONS" => "N",	// Использовать дополнительное ограничение доступа
-	"DISPLAY_DATE" => "N",	// Выводить дату элемента
-	"DISPLAY_NAME" => "N",	// Выводить название элемента
-	"DISPLAY_PICTURE" => "Y",	// Выводить детальное изображение
-	"DISPLAY_PREVIEW_TEXT" => "N",	// Выводить текст анонса
-	"USE_SHARE" => "N",	// Отображать панель соц. закладок
-	"PAGER_TEMPLATE" => "",	// Шаблон постраничной навигации
-	"DISPLAY_TOP_PAGER" => "N",	// Выводить над списком
-	"DISPLAY_BOTTOM_PAGER" => "N",	// Выводить под списком
-	"PAGER_TITLE" => "Страница",	// Название категорий
-	"PAGER_SHOW_ALL" => "N",	// Показывать ссылку "Все"
-	),
-	false
-);?>
-<div id="main-bottom">
-		<div class="content-frame">
-			<a href="/mineral_water.php" class="main-items">
-				<img src="/upload/main-item1.png">
-				<div class="circle-description" id="mineral-water">
-					<div class="circle-content">
-						<h1>Минеральная вода</h1>
-						<p>ООО «Быстра»</p>
-						<ul>
-							<li>ТМ «Серебряная»</li>
-							<li>ТМ «Аквада»</li>
-							<li>ТМ «Вода для всех»</li>
-						</ul>
-					</div>
-				</div>
-			</a>
-			<a href="/limonade.php" class="main-items">
-				<img src="/upload/main-item2.png">
-				<div class="circle-description" id="limonad">
-					<div class="circle-content">
-						<h1>Лимонады</h1>
-						<p>ООО «Аквадар»</p>
-						<ul>
-							<li>ТМ «Аквадар»</li>
-							<li>ТМ «Ситро»</li>
-						</ul>
-					</div>
-				</div>
-			</a>
-			<a href="/juice.php" class="main-items">
-				<img src="/upload/main-item3.png">
-				<div class="circle-description" id="juice">
-					<div class="circle-content">
-						<h1>Сокосодержащие напитки</h1>
-						<p>ООО «Аквадар»</p>
-						<ul>
-							<li>ТМ «Южный день»</li>
-						</ul>
-					</div>
-				</div>
-			</a>
-		</div>
-	</div><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:news.detail",
+	"main_page_top",
+	Array(
+		"IBLOCK_TYPE" => "content",
+		"IBLOCK_ID" => "7",
+		"ELEMENT_ID" => "45",
+		"ELEMENT_CODE" => "",
+		"CHECK_DATES" => "Y",
+		"FIELD_CODE" => array(0=>"",1=>"",),
+		"PROPERTY_CODE" => array(0=>"",1=>"",),
+		"IBLOCK_URL" => "",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"AJAX_OPTION_HISTORY" => "N",
+		"CACHE_TYPE" => "A",
+		"CACHE_TIME" => "36000000",
+		"CACHE_GROUPS" => "Y",
+		"META_KEYWORDS" => "-",
+		"META_DESCRIPTION" => "-",
+		"BROWSER_TITLE" => "-",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "N",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+		"ADD_SECTIONS_CHAIN" => "N",
+		"ADD_ELEMENT_CHAIN" => "N",
+		"ACTIVE_DATE_FORMAT" => "",
+		"USE_PERMISSIONS" => "N",
+		"DISPLAY_DATE" => "N",
+		"DISPLAY_NAME" => "N",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "N",
+		"USE_SHARE" => "N",
+		"PAGER_TEMPLATE" => "",
+		"DISPLAY_TOP_PAGER" => "N",
+		"DISPLAY_BOTTOM_PAGER" => "N",
+		"PAGER_TITLE" => "Страница",
+		"PAGER_SHOW_ALL" => "N"
+	)
+);?><?$APPLICATION->IncludeComponent(
+	"bitrix:news.list",
+	"main_page_catalog",
+	Array(
+		"IBLOCK_TYPE" => "content",
+		"IBLOCK_ID" => "8",
+		"NEWS_COUNT" => "3",
+		"SORT_BY1" => "SORT",
+		"SORT_ORDER1" => "ASC",
+		"SORT_BY2" => "ACTIVE_FROM",
+		"SORT_ORDER2" => "DESC",
+		"FILTER_NAME" => "",
+		"FIELD_CODE" => array(0=>"",1=>"",),
+		"PROPERTY_CODE" => array(0=>"ATT_CORPORATION",1=>"",),
+		"CHECK_DATES" => "Y",
+		"DETAIL_URL" => "",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"AJAX_OPTION_HISTORY" => "N",
+		"CACHE_TYPE" => "A",
+		"CACHE_TIME" => "36000000",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"ACTIVE_DATE_FORMAT" => "",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "N",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+		"ADD_SECTIONS_CHAIN" => "N",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"INCLUDE_SUBSECTIONS" => "N",
+		"DISPLAY_DATE" => "N",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"PAGER_TEMPLATE" => "",
+		"DISPLAY_TOP_PAGER" => "N",
+		"DISPLAY_BOTTOM_PAGER" => "N",
+		"PAGER_TITLE" => "Новости",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N"
+	)
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
