49bab889934c6cbfa3b98aa1520c937e976c7c2d
Andrey Chechkin <andrey.chechkin@web-izmerenie.ru>
2015-03-02 14:03:22 +0300

    Add backend contacts

---
+++ bitrix/templates/.default/components/bitrix/news.list/contacts/template.php
@@ -0,0 +1,36 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<ul>
+
+	<?foreach($arResult["ITEMS"] as $arItem):?>
+		<li>
+			<h3><?=$arItem["NAME"];?></h3>
+			<div class="map">
+				<?=$arItem["PROPERTIES"]["ATT_MAP"]["~VALUE"]["TEXT"];?>
+				<div class="info-map">
+					<div class="text">
+						<?=$arItem["PROPERTIES"]["ATT_ADRESS"]["~VALUE"]["TEXT"];?>
+						<p>
+							 Телефоны:<br>
+							 <?foreach ($arItem["PROPERTIES"]["ATT_TELEPHONE"]["VALUE"] as $telephone){
+								echo $telephone;
+								echo "<br>";
+							}?>
+						</p>
+						<p>
+							 Факс:<br>
+							 <?foreach ($arItem["PROPERTIES"]["ATT_FAKS"]["VALUE"] as $faks){
+								echo $faks;
+								echo "<br>";
+							}?>
+						</p>
+						<?foreach ($arItem["PROPERTIES"]["ATT_MAIL"]["VALUE"] as $mail){?>
+							<a href="mailto:<?=$mail;?>"><?=$mail?></a>
+						<?}?>
+					</div>
+				</div>
+			</div>
+		</li>
+
+	<?endforeach;?>
+
+</ul>
--- kontakty/index.php
+++ kontakty/index.php
@@ -1,85 +1,53 @@
 <?define("CONTACTS", "Y");
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Контакты");?>
-<ul>
-	<li>
-		<h3>ООО «Аквадар»</h3>
-		<div class="map">
-			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2706.4810096278766!2d39.781104000000006!3d47.28539900000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40e3b7a843a91339%3A0xc0631b070b4553cd!2z0JDQutCy0LDQtNCw0YA!5e0!3m2!1sru!2sru!4v1423211137593" width="950" height="450" frameborder="0" style="border:0"></iframe>
-			<div class="info-map">
-				<div class="text">
-					<p>г. Ростов-на-Дону ул. Орская, 23</p>
-					<p>Телефоны:</br>
-						+7(863) 223-35-35
-						+7(863) 223-35-38
-						+7(863) 223-35-33</p>
-					<p>Факс:</br>
-						+7 (863) 223-35-33</p>
-					<a href="mailto:mail@akvadar.ru">mail@akvadar.ru</a>
-					</div>
-				</div>
-			</div>
-	</li>
-	<li>
-		<h3>ООО «Быстра»</h3>
-		<div class="map">
-			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d21433.83124468137!2d41.0435791!3d47.81578145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x411d852079800aff%3A0x670a726851bced38!2z0KPRgdGC0Ywt0JHRi9GB0YLRgNGP0L3RgdC60LDRjywg0KDQvtGB0YLQvtCy0YHQutCw0Y8g0L7QsdC7Lg!5e0!3m2!1sru!2sru!4v1423211928450" width="950" height="450" frameborder="0" style="border:0"></iframe>
-			<div class="info-map">
-				<div class="text">
-					<p>346555</br>
-						Ростовская обл. Усть-Донецкий р-н
-						Станица Усть-Быстрянская</p>
-					<i>Примерно 1, 5 км
-						на юго-запад от ст. Усть-Быстрянская</i>
-					<p>Телефон:</br>
-						+7(863) 223-35-35<br>
-						+7(863) 223-35-38 </p>
-					<p>Факс:</br>
-						+7(863) 223-35-33</p>
-					<a href="mailto:bistra@akvadar.ru">bistra@akvadar.ru</a>
-				</div>
-			</div>
-		</div>
-	</li>
-	<li>
-		<h3>ООО «Легион»</h3>
-		<div class="map">
-			<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2706.128981215284!2d39.776769!3d47.292279!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40e3b7adc27ea559%3A0x486fd41bfa287827!2z0J7RgNGB0LrQsNGPINGD0LsuLCAzMdCQLCDQoNC-0YHRgtC-0LIt0L3QsC3QlNC-0L3Rgywg0KDQvtGB0YLQvtCy0YHQutCw0Y8g0L7QsdC7LiwgMzQ0MDY1!5e0!3m2!1sru!2sru!4v1423211706344" width="950" height="450" frameborder="0" style="border:0"></iframe>
-			<div class="info-map">
-				<div class="text">
-					<p>344065</br>
-						г. Ростов-на-Дону
-						ул. Орская, 31А, оф. 12 (2 этаж)</p>
-					<p>Телефоны:</br>
-						+7 (863) 290-64-52
-						+7 (863) 290-64-54</p>
-					<p>Факс:</br>
-						+7 (863) 290-64-53</p>
-					<a href="mailto:legion@akvadar.ru">legion@akvadar.ru</a>
-					<a href="mailto:opt_legion@akvadar.ru">opt_legion@akvadar.ru</a>
-				</div>
-			</div>
-		</div>
-	</li>
-	<li>
-		<h3>ООО «Акватика»</h3>
-		<div class="map">
-			<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2708.704128704948!2d39.687394999999995!3d47.241933!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40e3b8580ca8a563%3A0x99b9022a73feea58!2z0KLQsNCy0YDQuNGH0LXRgdC60LDRjyDRg9C7LiwgNCwg0KDQvtGB0YLQvtCyLdC90LAt0JTQvtC90YMsINCg0L7RgdGC0L7QstGB0LrQsNGPINC-0LHQuy4sIDM0NDAxMg!5e0!3m2!1sru!2sru!4v1423211864954" width="950" height="450" frameborder="0" style="border:0"></iframe>
-			<div class="info-map">
-				<div class="text">
-					<p>344012</br>
-						г. Ростов-на-Дону
-						ул. Таврическая, 4</p>
-					<p>Телефоны:</br>
-						+7 (863) 204-26-68
-						+7 (863) 266-10-04</p>
-					<p>Факс:</br>
-						+7 863) 232-14-39
-						+7 (863) 232-14-59</p>
-					<a href="mailto:ooo.akvatika@yandex.ru">ooo.akvatika@yandex.ru</a>
-				</div>
-			</div>
-		</div>
-	</li>
-</ul>
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+$APPLICATION->SetTitle("Контакты");?><?$APPLICATION->IncludeComponent(
+	"bitrix:news.list",
+	"contacts",
+	Array(
+		"IBLOCK_TYPE" => "content",
+		"IBLOCK_ID" => "22",
+		"NEWS_COUNT" => "20",
+		"SORT_BY1" => "SORT",
+		"SORT_ORDER1" => "ASC",
+		"SORT_BY2" => "ID",
+		"SORT_ORDER2" => "DESC",
+		"FILTER_NAME" => "",
+		"FIELD_CODE" => array(0=>"",1=>"",),
+		"PROPERTY_CODE" => array(0=>"ATT_MAIL",1=>"ATT_ADRESS",2=>"ATT_MAP",3=>"ATT_TELEPHONE",4=>"ATT_FAKS",5=>"",),
+		"CHECK_DATES" => "Y",
+		"DETAIL_URL" => "",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"AJAX_OPTION_HISTORY" => "N",
+		"CACHE_TYPE" => "A",
+		"CACHE_TIME" => "36000000",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"SET_TITLE" => "N",
+		"SET_BROWSER_TITLE" => "N",
+		"SET_META_KEYWORDS" => "N",
+		"SET_META_DESCRIPTION" => "N",
+		"SET_STATUS_404" => "N",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+		"ADD_SECTIONS_CHAIN" => "N",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"INCLUDE_SUBSECTIONS" => "N",
+		"DISPLAY_DATE" => "N",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "N",
+		"DISPLAY_PREVIEW_TEXT" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"DISPLAY_TOP_PAGER" => "N",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"PAGER_TITLE" => "Новости",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N"
+	)
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
