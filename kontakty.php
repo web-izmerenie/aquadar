1562b1298fec59c772dc356de48053ff8862ec71
Andrey Chechkin <andrey.chechkin@web-izmerenie.ru>
2015-03-11 18:02:06 +0300

    Edit content

--- kontakty.php
+++ kontakty.php
@@ -1,57 +1,84 @@
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Контакты");?>
-<section class="castom_contacts">
+$APPLICATION->SetTitle("Контакты");?><section class="castom_contacts">
+<ul>
+	<li>
+	<h2>Ростов-на-Дону</h2>
+	<p>
+		ул. Орская, 23
+	</p>
+	<p>
+		+7 (863) 223-35-35
+	</p>
+	<p><a href="mailto:rnd@akvadar.ru">rnd@akvadar.ru</a></p>
+	<p><a href="mailto:opt@akvadar.ru">opt@akvadar.ru</a></p>
+ </li>
+	<li>
+	<h2>Санкт-Петербург</h2>
+	<p>
+		ул. Гапсальская, 5а, оф. 507
+	</p>
+	<p>
+		+7 (812) 622-13-28
+	</p>
+	<p><a href="mailto:spb@akvadar.ru">spb@akvadar.ru</a></p>
+ </li>
+	<li>
+	<h2>Тула</h2>
+	<p>
+		ул. Каракозова, 75, оф. 15
+	</p>
+	<p>
+		+7 (4872) 24-75-99
+	</p>
+	<p><a href="mailto:tula@akvadar.ru">tula@akvadar.ru</a></p>
+ </li>
+	<li>
+	<h2>Ставрополь</h2>
+	<p>
+		ул. К. Маркса, 63
+	</p>
+	<p>
+		+7 (8652) 21-36-15
+	</p>
+	<p><a href="mailto:stavropol@akvadar.ru">stavropol@akvadar.ru</a></p>
+ </li>
+	<li>
+	<h2>Самара</h2>
+	<p>
+		ул. Алексея Толстого, 92
+	</p>
+	<p>
+		+7 (846) 212-73-51
+	</p>
+	<p><a href="mailto:samara@akvadar.ru">samara@akvadar.ru</a></p>
+ </li>
+	<li>
+	<h2>Махачкала</h2>
+	<p>
+		ул. Гагарина, 12
+	</p>
+	<p>
+		+7 (8722) 55-13-52
+	</p>
+	<p><a href="mailto:maxachkala@akvadar.ru">maxacgkala@akvadar.ru</a></p>
+ </li>
+</ul>
+ </section> <section class="rekvesits"> <a href="#rekvesits">Реквизиты</a>
+<div class="hide-info">
+	<h3>ООО «Аквадар»</h3>
+	<p>
+		Адрес юрид. : 344016 г. Ростов-на-Дону, пер. Элеваторный, 2<br>
+		 Адрес факт.: 344065, г. Ростов-на-Дону, ул. Орская, 23
+	</p>
+	<p>
+		Телефон/факс/E-Mail: (863) 223-35-35,<br>
+ <a href="mailto:mail@akvadar.ru">mail@akvadar.ru</a>
+	</p>
 	<ul>
-		<li>
-			<h2>Ростов-на-Дону</h2>
-			<p>ул. Орская, 23</p>
-			<p>+7 (863) 223-35-35</p>
-		</li>
-		<li>
-			<h2>Санкт-Петербург</h2>
-			<p>ул. Гапсальская, 5а, оф. 507</p>
-			<p>+7 (812) 622-13-28</p>
-		</li>
-		<li>
-			<h2>Тула</h2>
-			<p>ул. Каракозова, 75, оф. 15</p>
-			<p>+7 (4872) 24-75-99</p>
-		</li>
-		<li>
-			<h2>Ставрополь</h2>
-			<p>ул. К. Маркса, 63</p>
-			<p>+7 (8652) 21-36-15</p>
-		</li>
-		<li>
-			<h2>Самара</h2>
-			<p>ул. Алексея Толстого, 92</p>
-			<p>+7 (846) 212-73-51</p>
-		</li>
-		<li>
-			<h2>Махачкала</h2>
-			<p>ул. Гагарина, 12</p>
-			<p>+7 (8722) 55-13-52</p>
-		</li>
+		<li>Расчетный счет: 40702810500400001850</li>
+		<li>Наименование банка: ОАО КБ «Центр – Инвест» г. Ростов-на-Дону</li>
+		<li>Корреспондентский счет: 30101810100000000762</li>
+		<li>БИК: 046015762</li>
 	</ul>
-</section>
-
-<section class="rekvesits">
-	<a href="#rekvesits">Реквезиты</a>
-	<div class="hide-info">
-		<h3>ООО «Аквадар»</h3>
-
-		<p>Адрес юрид. : 344016 г. Ростов-на-Дону, пер. Элеваторный, 2</br>
-		Адрес факт.:  344065, г. Ростов-на-Дону, ул. Орская, 23</p>
-
-		<p>Телефон/факс/E-Mail: (863) 223-35-35,</br>
-		<a href="mailto:mail@akvadar.ru">mail@akvadar.ru</a></p>
-		<ul>
-			<li>Расчетный счет: 40702810500400001850</li>
-			<li>Наименование банка: ОАО КБ «Центр – Инвест» г. Ростов-на-Дону</li>
-			<li>Корреспондентский счет: 30101810100000000762</li>
-			<li>БИК: 046015762</li>
-		</ul>
-	</div>
-</section>
-
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+</div>
+ </section><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
