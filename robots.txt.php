a4eff064e1194f3416da5d978797606bc0dd9b3e
Andrey Chechkin <andrey.chechkin@web-izmerenie.ru>
2016-07-21 12:02:43 +0300

    Edit robots

--- robots.txt.php
+++ robots.txt.php
@@ -1,3 +1,8 @@
-User-agent: *
-Disallow: /administrator/
+<?if($_SERVER["HTTP_HOST"] == "ust-bistra.ru" || $_SERVER["HTTP_HOST"] == "www.ust-bistra.ru"){?>
+	User-agent: *
+	Disallow: /
+<?} else {?>
+	User-agent: *
+	Disallow: /administrator/
+}?>
 Host: <?php echo trim($_SERVER['HTTP_HOST']);?>
